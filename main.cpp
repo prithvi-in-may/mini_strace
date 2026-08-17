c5c38d0fcdebb6dd1b7fbc7061e6e7261dc6df02
prithvi-in-may <prithvi.in.may@gmail.com>
2026-08-17 23:11:42 +0530

    Allow the child to run before parent dies

--- main.cpp
+++ main.cpp
@@ -21,7 +21,8 @@ int main(int argc, char * argv[]) {
     cout << "I am the child" << endl;
 
     // Using ptrace option PTRACE_TRACEME to allow the parent process trace the child process and puts a `tracing stop` after execvp is executed 
-
+    // ptrace(WHAT_DO_I_WANT, WHICH_PROCESS, ADDRESS, EXTRA_DATA)
+    // ptrace second argument is the process id which must be passed in other options except PTRACE_TRACEME
     ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
 
     // argv[1] gives value at index 1, &argv[1] gives value of memory address at index 1 which is an array
@@ -45,6 +46,17 @@ int main(int argc, char * argv[]) {
 
     cout << "Signal: " << WSTOPSIG(status);
     cout << " (" << strsignal(WSTOPSIG(status)) << ")" << endl;
+
+    // Allow the child to run before the parent dies, the child will run anyway if the tracer (parent process) disappears though
+    ptrace(PTRACE_CONT, pid, nullptr, nullptr);
+
+    waitpid(pid, &status, 0);
+
+    cout << "Child finished" << endl;
+
+    if (WIFEXITED(status)) {
+      cout << "Child exited with status: " << WIFEXITED(status) << endl;
+     }
 
   } else {
     perror("Fork");
