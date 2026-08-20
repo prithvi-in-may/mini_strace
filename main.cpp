57603b0c10dc54b2a9f5d8132a1d0713f2632cc8
prithvi-in-may <prithvi.in.may@gmail.com>
2026-08-20 14:14:14 +0530

    Removed signal, code, syscall number code block and fixed the code for entry and exit syscall stops

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-int main(int argc, char * argv[]) { 
-  
+int main(int argc, char * argv[]) {
+
   pid_t pid = fork();
 
   if (pid == 0) {
@@ -22,7 +22,7 @@ int main(int argc, char * argv[]) {
 
     cout << "I am the child" << endl;
 
-    // Using ptrace option PTRACE_TRACEME to allow the parent process trace the child process and puts a `tracing stop` after execvp is executed 
+    // Using ptrace option PTRACE_TRACEME to allow the parent process trace the child process and puts a `tracing stop` after execvp is executed
     // ptrace(WHAT_DO_I_WANT, WHICH_PROCESS, ADDRESS, EXTRA_DATA)
     // ptrace second argument is the process id which must be passed in other options except PTRACE_TRACEME
     ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
@@ -30,8 +30,8 @@ int main(int argc, char * argv[]) {
     // argv[1] gives value at index 1, &argv[1] gives value of memory address at index 1 which is an array
     execvp(argv[1], &argv[1]);
     // After the function is executed, Kernel will stop it with `SIGTRAP` signal as part of tracing mechanism.
-    
-     // If execlp fails 
+
+     // If execlp fails
     perror("execlp");
 
   } else if (pid > 0) {
@@ -40,7 +40,7 @@ int main(int argc, char * argv[]) {
     cout << "I am the parent" << endl;
 
     // Status stores what state change happened to the child
-    
+
     int status;
     waitpid(pid, &status, 0);
 
@@ -50,6 +50,9 @@ int main(int argc, char * argv[]) {
     cout << " (" << strsignal(WSTOPSIG(status)) << ")" << endl;
 
     // Allow the child to run before the parent dies, the child will run anyway if the tracer (parent process) disappears though
+    bool entering_syscall = true; 
+    siginfo_t info;
+    struct user_regs_struct regs;
     while (true) {
     // A system call has two moments where parent can stop the child, first is when there is syscall entry and then the other one exit and PTRACE_SYSCALL stops at both. For producing real output, we need information from both sides.
     ptrace(PTRACE_SYSCALL, pid, nullptr, nullptr);
@@ -57,33 +60,28 @@ int main(int argc, char * argv[]) {
     waitpid(pid, &status, 0);
 
     cout << " " << endl;
-    cout << "ptrace stop" << endl;
-
-    if (WIFSTOPPED(status)) {
-      siginfo_t info; 
-      struct user_regs_struct regs;
-
-      ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
-      ptrace(PTRACE_GETSIGINFO, pid, nullptr, &info);
-
-      cout << "Syscall number: " << regs.orig_rax << endl;
-
-      // si_signo tells which signal occured (e.g. SIGTRAP)
-      cout << "Signal: " << info.si_signo << endl;
-      // si_code tells why that signal was generated or the metadata of the signal not syscall
-      cout << "Code: " << info.si_code << endl;
-     } 
-
+    
     if (WIFEXITED(status)) {
       break;
-    }} 
+    }
+    
+    if (entering_syscall) {
+      cout << "SYSCALL ENTRY" << endl;
+      cout << "Number: " << regs.orig_rax << endl;
+    } else {
+      cout << "SYSCALL EXIT" << endl;
+      cout << "Number: " << regs.orig_rax << endl;
+      cout << "Return value: " << regs.rax << endl;
+    }
 
+    entering_syscall = !entering_syscall;
+
+    }
 
   } else {
     perror("Fork");
   }
-  
+
   return 0;
 
 }
-
